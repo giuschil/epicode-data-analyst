390790498c366d8c3d16e2dde04425f0f4aed74d
Giuseppe Schillaci <39010839+giuschil@users.noreply.github.com>
2022-10-04 17:46:09 +0200

    Update esercizio3.py

--- Week2/Day2/esercizio3.py
+++ Week2/Day2/esercizio3.py
@@ -8,40 +8,29 @@ Quindi visualizza il costo per 100 km e quanta distanza può percorrere l'auto c
 """
 """
 #consumo = distanza/litri
-
 #litri_serbatoio = int(input("dimmi i litri del serbatio: "))
 #prezzo = float(input("dimmi prezzo benzina: "))
 #costo_viaggio =litri_serbatoio * prezzo * distanza
 #consumo =distanza/litri_serbatoio
-
 #efficienza = int(input("dimmi quanto km fai con un litro di benzina: "))
-
 #print("Il costo del viaggio è: ", costo_viaggio, "€")
-
-
 #consumo = efficienza * litri_serbatoio
-
 #print("la distanza che può percorrere l'auto con la benzina nel serbatoio è ", consumo)
-
 #costo_viaggio = 100km (distanza(km)) * (1/efficienza(km/litri)) * prezzo (euro/litri)
-
 """
 
 efficienza = int(input("inserisci efficienza: "))
 distanza = 100
+
 consumo = 1/efficienza
 prezzo = float(input("inserisci prezzo: "))
 costo = distanza * (1/(efficienza))* prezzo
 
-
-print("il costo del viaggio di 100km è: ", costo, "€")
+print("il costo del viaggio di 100km è: %", costo, "€")
 
 serbatoio = float(input("inserisci litri serbatoio: "))
+costo_viaggio = efficienza * prezzo * serbatoio
+km_rimasti  = efficienza * serbatoio
 
-km_rimasti = efficienza * prezzo * serbatoio
-costo_viaggio = efficienza * serbatoio
 print(km_rimasti)
 print(costo_viaggio)
-
-
-
