9ac8b4be84c7983d8e408c11bfd050beb59eb07f
Giuseppe Schillaci <39010839+giuschil@users.noreply.github.com>
2022-10-05 16:14:50 +0200

    Update esercizio1.py

--- Week2/Day3/esercizio1.py
+++ Week2/Day3/esercizio1.py
@@ -4,41 +4,32 @@ Esercizio 1: scrivi un programma che trasforma in maiuscolo tutte le consonanti 
 
 stringa = 'Totti Blasi, Ilary infuriata perché il Capitano non ha salvato le apparenze con Noemi'
 
+#trasforma in lettere maiuscole
 stringa = stringa.upper()
+
+#trasforma le sole vocali in minuscolo
 stringa = stringa.replace('A', 'a')
 stringa = stringa.replace('E', 'e')
 stringa = stringa.replace('I', 'i')
 stringa = stringa.replace('O', 'o')
 stringa = stringa.replace('U', 'u')
 
-
+#trasformo la stringa in una lista
 lista_parole = list(stringa)
+
+#faccio split delle parole usa lo spazio come separatore
 frase_split = stringa.split(' ')
 
+#crea una lista di vocali 
 vocali = ['a', 'e', 'i', 'o', 'u']
 
-"""
-dizionario = {
-    'a': ('parola', 'alba'),
-    'e': ('giuseppe', 'erba'),
-    'i': (),
-    'o': (),
-    'u': ()
-}
-"""
-"""
-vocale_a = 0
-vocale_e = 0
-vocale_i = 0
-vocale_o = 0
-vocale_u = 0
-"""
-
+#crera delle liste vuote una per ogni lettera dove inseriro tutte le parole che contengono quella vocale
 parola_con_a = []
 parola_con_e = []
 parola_con_i = []
 parola_con_o = []
 parola_con_u = []
+
 dizionario = {
     'a': [],
     'e': [],
@@ -47,45 +38,31 @@ dizionario = {
     'u': []
 }
 
+#facciamo un ciclo per ogni vocale in frase_split 
 for i in range(0, len(frase_split)):
-    # print(frase_split[i])
     if 'a' in frase_split[i]:
-        #vocale_a =vocale_a + 1
         parola_con_a.append(frase_split[i])
+#aggiungo le parole che hanno la a alla chiave 'a' nel dizionario
 dizionario["a"] = tuple(parola_con_a)
 
 for i in range(0, len(frase_split)):
-    # print(frase_split[i])
     if 'e' in frase_split[i]:
-        #vocale_e = vocale_e + 1
         parola_con_e.append(frase_split[i])
 dizionario["e"] = tuple(parola_con_e)
 
 for i in range(0, len(frase_split)):
-    # print(frase_split[i])
     if 'i' in frase_split[i]:
-        #vocale_i = vocale_i + 1
         parola_con_i.append(frase_split[i])
 dizionario["i"] = tuple(parola_con_i)
 
 for i in range(0, len(frase_split)):
-    # print(frase_split[i])
     if 'o' in frase_split[i]:
-        #vocale_o = vocale_o + 1
         parola_con_o.append(frase_split[i])
 dizionario["o"] = tuple(parola_con_o)
 
 for i in range(0, len(frase_split)):
-    # print(frase_split[i])
     if 'u' in frase_split[i]:
-        #vocale_u = vocale_u + 1
         parola_con_u.append(frase_split[i])
 dizionario["u"] = tuple(parola_con_u)
 
-"""
-print("lettera a",parola_con_a)
-print("lettera e", parola_con_e)
-print("lettera i",parola_con_i)
-print("lettera o", parola_con_o)
-print("lettera u", parola_con_u)
-"""
+print(dizionario)
